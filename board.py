95cea1974fa9c57e73907166be892a9087246ac1
Leo Brucker <leo.brucker@carrybots.de>
2024-11-25 22:38:49 +0100

    Add horizontal and diagonal movement

--- board.py
+++ board.py
@@ -1,33 +1,55 @@
 
 from utils import Position, Util
 from pieces import Piece, PieceType
-from typing import Optional, List
+from typing import Optional, List, Any
 
 
 class Board:
     def __init__(self):
         self.board = [[None for i in range(8)] for j in range(8)]
+        self.highlights = [[False for i in range(8)] for j in range(8)]
         self.util = Util()
         self.movement = Movement(self)
-        self._setup_board()
+        #self._setup_board()
 
     
-    def get_cell(self, pos : Position) -> Optional[Piece]:
+    def get_cell(self, pos : Position, board: List = None) -> Optional[Piece]:
+        board = self.board if not board else board
         if not pos.is_valid():
             self.util.print_warning(f"Cell at {pos} is not valid! This should never happen!")
             return None
-        return self.board[pos.y][pos.x]
+        return board[pos.y][pos.x]
     
     def get_pieces(self) -> List[Piece]:
         pieces = []
-        for row in self.board:
-            for piece in row:
-                if piece:
-                    pieces.append(piece)
+        for pos in self._all_positions():
+            piece = self.get_cell(pos)
+            if piece:
+                pieces.append(piece)
         return pieces
 
-    def _set_cell(self, value: Optional[Piece], pos: Position):
-        self.board[pos.y][pos.x] = value
+    def get_possible_moves(self, piece: Piece):
+        return self.movement.get_possible_moves(piece)
+
+    def highlight_cells(self, cells: List[Position]):
+        self._reset_highlights()
+        for cell in cells:
+            self._set_cell(True, cell, self.highlights)
+    
+    def _all_positions(self) -> List[Position]:
+        positions = []
+        for x in range(8):
+            for y in range(8):
+                positions.append(Position(x, y))
+        return positions
+    
+    def _reset_highlights(self):
+        for pos in self._all_positions():
+            self._set_cell(False, pos, self.highlights)
+
+    def _set_cell(self, value : Any, pos: Position, board: List = None):
+        board = self.board if not board else board
+        board[pos.y][pos.x] = value
     
     def _place_piece(self, piece: Piece, pos: Position) -> bool:
         if self.get_cell(pos):
@@ -64,17 +86,25 @@ class Board:
     
     def __str__(self):
         string = ""
-        for row in self.board:
-            for piece in row:
-                if piece:
-                    string += f"{str(piece)}"
+        for y, row in enumerate(self.board):
+            string += f" {str(y)} |"
+            for x, piece in enumerate(row):
+                highlight = self.get_cell(Position(x, y), self.highlights)
+                if highlight:
+                    start_format = "\033[1;33m"  # Bold and orange
+                    end_format = "\033[0m"  # Reset to default
                 else:
-                    string += " "
-                string += " |"
-            string = string[:-1] + "\n"
-        return string
-        
-
+                    start_format = ""
+                    end_format = ""
+                if piece:
+                    string += f"{start_format}{str(piece)} {end_format}"
+                else:
+                    cell = "◉︎" if highlight else "  "
+                    string += f"{start_format}{cell}{end_format}"
+                string += "|"
+            string = string + "\n"
+        string += "    a  b  c  d  e  f  g  h"
+        return string 
 class Movement():
     def __init__(self, board : Board):
         self.board = board
@@ -84,14 +114,33 @@ class Movement():
         params = MovementParameters(piece_type=piece.type)
         cells = []
         if params.horizontal:
-            pos = piece.pos
-            stop = False
-            while not stop:
-                pos += Position(1, 0)
-                #if pos.is_valid() ...
-                #self.board.get_cell(pos + offset)
-                pass
+            cells.extend(self._get_cells_in_direction(piece.pos, Position(1,0), piece.white))
+            cells.extend(self._get_cells_in_direction(piece.pos, Position(-1,0), piece.white))
+            cells.extend(self._get_cells_in_direction(piece.pos, Position(0,1), piece.white))
+            cells.extend(self._get_cells_in_direction(piece.pos, Position(0,-1), piece.white))
+        if params.diagonal:
+            cells.extend(self._get_cells_in_direction(piece.pos, Position(1,1), piece.white))
+            cells.extend(self._get_cells_in_direction(piece.pos, Position(1,-1), piece.white))
+            cells.extend(self._get_cells_in_direction(piece.pos, Position(-1,1), piece.white))
+            cells.extend(self._get_cells_in_direction(piece.pos, Position(-1,-1), piece.white))
+        return cells
 
+    def _get_cells_in_direction(self, start_pos: Position, direction: Position, white: bool) -> List[Position]:
+        pos = start_pos
+        cells = []
+        while True: 
+            pos += direction
+            if not pos.is_valid():
+                break
+            piece = self.board.get_cell(pos)
+            if piece:
+                if piece.white == white: 
+                    break
+                else:
+                    cells.append(pos)
+                    break
+            cells.append(pos)
+        return cells
 
 
 class MovementParameters():
--- game.py
+++ game.py
@@ -6,10 +6,25 @@ from pieces import Piece, PieceType
 pos1 = Position(1,2)
 board = Board()
 
-print(board)
 
+board._place_piece(Piece(PieceType.ROOK, True), Position(6, 2))
+board._place_piece(Piece(PieceType.KNIGHT, True), Position(6, 3))
+board._place_piece(Piece(PieceType.BISHOP, True), Position(2, 7))
+board._place_piece(Piece(PieceType.QUEEN, True), Position(3, 2))
+board._place_piece(Piece(PieceType.ROOK, False), Position(1, 1))
+board._place_piece(Piece(PieceType.KNIGHT, False), Position(4, 4))
+board._place_piece(Piece(PieceType.BISHOP, False), Position(6, 5))
+board._place_piece(Piece(PieceType.QUEEN, False), Position(4, 2))
+
+print(board)
 pieces = board.get_pieces()
-print(f"piece: \n{str(pieces[0])}")
-board.movement.get_possible_moves(piece=pieces[0])
+print()
+for piece in pieces:
+    print(f"piece: \n{str(piece)}")
+    moves = board.get_possible_moves(piece=piece)
+    board.highlight_cells(moves)
+    print(board)
+    print()
+
 
 
